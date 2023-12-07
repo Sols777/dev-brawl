e3130be3e5fa9150fc376e7e96e261c37ffa6210
luis <luisportelatorres@gmail.com>
2023-12-07 11:22:20 +0000

    leaderboards page now show best times per language

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,14 +7,33 @@ class PagesController < ApplicationController
   def leaderboards
     @languages = Language.all
     @user = current_user
-    @users_score = User.order(score: :desc)
-    @users_time = User.includes(:submissions).order("submissions.time_taken ASC").distinct
+    # Build a hash to store language-specific data
+    @languages_data = {}
+    @languages.each do |language|
+      language_data = Language.joins(challenges: { submissions: :user })
+                              .select('languages.name AS language_name, MIN(submissions.time_taken) AS best_time, users.username AS best_username')
+                              .where(id: language.id)
+                              .group('languages.id, users.username')
+                              .preload(challenges: { submissions: :user })
+                              .order('best_time ASC')
+
+      # Store language-specific data in the hash
+      @languages_data[language.id] = language_data
+    end
   end
+
+
+
+
 
   def mydashboard
     @languages = Language.all
     @users_score = User.order(score: :desc)
     @users_time = User.includes(:submissions).order("submissions.time_taken ASC").distinct
+  end
+
+  def best_users
+    challenges.flat_map { |challenge| challenge.submissions.min_by(&:time_taken)&.user }
   end
 
   def profile
@@ -24,6 +43,7 @@ class PagesController < ApplicationController
   end
 
   private
+
 
   def calculate_win_percentage(submissions)
     return 0 if submissions.empty?
